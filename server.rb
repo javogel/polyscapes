930e4a560b9d9e9d17104a52cabdef36d717b27f
Johannus Vogel <johannus.vogel@gmail.com>
2020-01-06 23:03:51 -0800

    auto reload only on development mode

--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-also_reload '/public/sketch.js'
+also_reload '/public/sketch.js' if development?
 
 set :static_cache_control, [:public, max_age: 0]
 
